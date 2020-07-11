2f2c436b0656b90c378349bbdbaf99f2074f2c51
Andrey Slukin <aslukin@gmail.com>
2020-07-12 01:47:28 +0200

    finaized layout+icons

--- src/Components/Todo-List-Item/Todo-List-Item.js
+++ src/Components/Todo-List-Item/Todo-List-Item.js
@@ -1,17 +1,25 @@
 import React from 'react'
 import './Todo-List-Item.css'
 
-const TodoListItem = ({label, important = false, id}) => {
+const TodoListItem = ({ label, important = false, id }) => {
     const style = {
         color: important ? 'tomato' : 'black'
     }
     return (
-        <span className = "todo-list-item" style = {style}>            
-
-            {label}
-            
-        </span>
-
+        <div className="todo-list-item">
+            <div className="todo-list-item-label" style={style}>
+                {label}
+            </div>
+            <div>
+                <button type="button" className="btn btn-outline-warning todo-list-item-button">
+                    <i className="fa fa-exclamation" />
+                </button>
+                <button type="button" className="btn btn-outline-danger todo-list-item-button">
+                    {/* <i className="fa fa-archive"></i> */}
+                    <i className="fa fa-trash"></i>
+                </button>
+            </div>
+        </div>
     )
 }
 export default TodoListItem
